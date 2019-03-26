0d0dc1ee17dc5b416c3f8e34c8f5f49bc6300db8
Pablo Costanzo <costanzopa@gmail.com>
2019-03-26 19:55:19 -0300

    Using Dynamic pages

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
 
-const path = require('path');
+const path = require('path')
+const express = require('express')
 const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
-const express = require('express');
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-const app = express();
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
 
-const publicDirectoryPath = path.join(__dirname, '../public');
-app.use(express.static(publicDirectoryPath));
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Pablo Costanzo'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Pablo Costanzo'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
 
 app.get('/weather', (req, res) => {
-	geocode("Buenos Aires", (error, {latitude, longitude, location}) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
+
+/*geocode("Buenos Aires", (error, {latitude, longitude, location}) => {
 		if (error) {
 			res.send(error);
 		} else {
@@ -23,11 +53,6 @@ app.get('/weather', (req, res) => {
 				}
 			})
 		}
-	})	
-});
-
-app.listen(3000, () => {
-	console.log('Server is up on port 3000.');
-})
+	})*/	
 
 	
