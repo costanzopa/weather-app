0ef6698563e461dcb5d01daedd3b2591fcc18d16
Pablo Costanzo <costanzopa@gmail.com>
2019-03-26 15:11:00 -0300

    Refactoring: Destructuring

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const address = process.argv[2];
 if (!address) {
 	console.log('Please provide an address.');
 } else {
-	geocode(address, (error, data) => {
+	geocode(address, (error, {latitude, longitude, location}) => {
 		if (error) {
 			return	console.log(error);
 		} else {
-			forecast(data.longitude, data.latitude, (error, forecastData) => {
+			forecast(longitude, latitude, (error, forecastData) => {
 				if (error) {
 					return console.log(error);
 				}
-				console.log(data.location);
+				console.log(location);
 				console.log(forecastData);
 			})
 		}
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-	const url = 'https://api.darksky.net/forecast/ca4efca8186006eb84e3288ec81472a3/'+latitude+','+longitude;
+	const url = 'https://api.darksky.net/forecast/ca4efca8186006eb84e3288ec81472a3/'+latitude+','+longitude+'?lang=es&units=si';
 
-	request({url: url, json: true}, (error, response) => {
+	request({url, json: true}, (error, { body }) => {
 		if (error) {
 			callback('Unable to connect to location services!.', undefined);
-		} else if (response.body.error) {
+		} else if (body.error) {
 			callback('Unable to find location. Try another search.', undefined);
 		} else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
 		}
 	})
 }
