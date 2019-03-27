7373130a5ce108b32794f3298074918928168242
costanzopa <nitopatapufete11>
2019-03-27 14:47:03 -0300

    Adding fetch to the client.

--- src/app.js
+++ src/app.js
@@ -27,6 +27,32 @@ app.get('', (req, res) => {
     })
 })
 
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
@@ -60,19 +86,4 @@ app.get('/weather', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
-
-/*geocode("Buenos Aires", (error, {latitude, longitude, location}) => {
-		if (error) {
-			res.send(error);
-		} else {
-			forecast(longitude, latitude, (error, forecastData) => {
-				if (error) {
-					res.send(error);
-				} else  {
-					res.send(location +"\n" + forecastData);
-				}
-			})
-		}
-	})*/	
-
 	
