ae1a2fd44cfbe343f311ee729ad87f7e927c6199
costanzopa <nitopatapufete11>
2019-03-27 17:43:45 -0300

    Final Styling Weather app

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-	response.json().then((data) => {
-		if(data.error) {
-			console.log(data.error);
-		} else {
-			console.log(data.location);
-			console.log(data.forecast);
-		}
+var weatherForm  = document.querySelector('form');
+var search = document.querySelector('input');
+var firstMessage = document.querySelector('#firstMessage');
+var secondMessage = document.querySelector('#secondMessage');
+
+weatherForm.addEventListener('submit',(e) => {
+	e.preventDefault();
+
+	firstMessage.textContent = "Loading..."
+	secondMessage.textContent = "";
+	const location = search.value;
+
+	fetch('http://localhost:3000/weather?address='+location).then((response) => {
+		response.json().then((data) => {
+			if(data.error) {
+				firstMessage.textContent = data.error;
+			} else {
+				firstMessage.textContent = data.location;
+				secondMessage.textContent = data.forecast;
+			}
+		})
 	})
 })
