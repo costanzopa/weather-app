9a0090fd1557a87345f28631a0e82aa09fa48182
Pablo Costanzo <costanzopa@gmail.com>
2019-03-27 22:39:14 -0300

    Adding more weather information.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-	const url = 'https://api.darksky.net/forecast/ca4efca8186006eb84e3288ec81472a3/'+latitude+','+longitude+'?lang=es&units=si';
+	const url = 'https://api.darksky.net/forecast/ca4efca8186006eb84e3288ec81472a3/'+latitude+','+longitude+'?units=si';
 
 	request({url, json: true}, (error, { body }) => {
 		if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
 		} else if (body.error) {
 			callback('Unable to find location. Try another search.', undefined);
 		} else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
 		}
 	})
 }
